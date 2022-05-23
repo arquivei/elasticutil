1002a49f2d9c410407afe8275b3b0c5d65d779d3
marcosbmf <marcosbarrosmf@gmail.com>
2022-05-23 17:07:25 -0300

    Add entities for Range, Nested and FullTextSearch. Add errors.

---
+++ entities.go
@@ -0,0 +1,51 @@
+package elasticutil
+
+import "time"
+
+// TimeRange represents a time range with a beginning and an end
+type TimeRange struct {
+	From time.Time
+	To   time.Time
+}
+
+// IntRange represents an int range with a beginning and an end
+type IntRange struct {
+	From uint64
+	To   uint64
+}
+
+// FloatRange represents a float range with a beginning and an end
+type FloatRange struct {
+	From float64
+	To   float64
+}
+
+// Nested represents a nested query
+type Nested struct {
+	payload interface{}
+}
+
+// NewNested creates a Nested struct with the given payload
+func NewNested(payload interface{}) Nested {
+	return Nested{payload}
+}
+
+// FullTextSearchMust Represents a Must's Full Text Search
+type FullTextSearchMust struct {
+	payload interface{}
+}
+
+// NewFullTextSearchMust creates a FullTextSearchMust struct with the given payload.
+func NewFullTextSearchMust(payload interface{}) FullTextSearchMust {
+	return FullTextSearchMust{payload}
+}
+
+// FullTextSearchMust Represents a Should's Full Text Search
+type FullTextSearchShould struct {
+	payload interface{}
+}
+
+// NewFullTextSearchShould creates a FullTextSearchShould struct with the given payload.
+func NewFullTextSearchShould(payload interface{}) FullTextSearchShould {
+	return FullTextSearchShould{payload}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module bitbucket.org/arquivei/elasticutil
 
 go 1.18
+
+require github.com/arquivei/foundationkit v0.1.2
+
+require github.com/rs/zerolog v1.26.0 // indirect
