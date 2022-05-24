29debdd1de98b22a97bd8f845126b4523e1808ef
marcosbmf <marcosbarrosmf@gmail.com>
2022-05-24 11:06:07 -0300

    Add utility functions to extract response/error from search results.

---
+++ response.go
@@ -0,0 +1,69 @@
+package elasticutil
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/arquivei/foundationkit/errors"
+	"github.com/olivere/elastic/v7"
+)
+
+// AllShardsMustReplyOnElasticSearch AllShardsMustReplyOnElasticSearch
+func AllShardsMustReplyOnElasticSearch(
+	results *elastic.SearchResult,
+	err error,
+) (*elastic.SearchResult, error) {
+	if err == nil &&
+		results != nil &&
+		results.Shards != nil &&
+		results.Shards.Failed > 0 {
+		err = errors.E(
+			ErrNotAllShardsReplied,
+			errors.KV("replied", results.Shards.Successful),
+			errors.KV("failed", results.Shards.Failed),
+			errors.KV("total", results.Shards.Total),
+		)
+	}
+	return results, err
+}
+
+// GetErrorFromElasticResponse GetErrorFromElasticResponse
+func GetErrorFromElasticResponse(err error) error {
+	switch e := err.(type) {
+	case *elastic.Error:
+		err = errors.New(getRootCauseFromElasticError(e.Details))
+	}
+	return err
+}
+
+func getRootCauseFromElasticError(errorDetails *elastic.ErrorDetails) string {
+	if errorDetails == nil {
+		return "unknown elastic error"
+	}
+	errors := []string{errorDetails.Type + "[" + errorDetails.Reason + "]"}
+
+	for _, rootCause := range errorDetails.RootCause {
+		errors = append(errors, rootCause.Type+"["+rootCause.Reason+"]")
+	}
+
+	return strings.Join(errors, ": ")
+}
+
+// GetElasticPaginatorFromHits GetElasticPaginatorFromHits
+func GetElasticPaginatorFromHits(hits []*elastic.SearchHit) (string, error) {
+	const op errors.Op = "elasticutil.GetElasticPaginatorFromHits"
+
+	if len(hits) == 0 {
+		return "", nil
+	}
+
+	lastHit := hits[len(hits)-1]
+	if lastHit == nil || lastHit.Sort == nil {
+		return "", nil
+	}
+	paginator, err := json.Marshal(lastHit.Sort)
+	if err != nil {
+		return "", errors.E(op, err)
+	}
+	return string(paginator), nil
+}
