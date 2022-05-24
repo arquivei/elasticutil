e31ec3037b8a88f02ae1aac9d263883c8c116c1f
Marcos Barros <34271729+marcosbmf@users.noreply.github.com>
2022-05-24 15:14:54 -0300

    [Feature] Response Utility Functions (#3)
    * Add utility functions to extract response/error from search results.
    
    * Add tests and string docs to response.go functions.

--- go.mod
+++ go.mod
@@ -4,4 +4,17 @@ go 1.18
 
 require github.com/arquivei/foundationkit v0.1.2
 
-require github.com/rs/zerolog v1.26.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
+
+require (
+	github.com/olivere/elastic/v7 v7.0.32
+	github.com/rs/zerolog v1.26.0 // indirect
+	github.com/stretchr/testify v1.7.1
+)
---
+++ response.go
@@ -0,0 +1,69 @@
+package elasticutil
+
+import (
+	"encoding/json"
+	"strings"
+
+	"github.com/arquivei/foundationkit/errors"
+	"github.com/olivere/elastic/v7"
+)
+
+// AllShardsMustReplyOnElasticSearch checks if any shard failed to respond.
+func AllShardsMustReplyOnElasticSearch(
+	results *elastic.SearchResult,
+	err error,
+) (*elastic.SearchResult, error) {
+	if err == nil &&
+		results != nil &&
+		results.Shards != nil &&
+		results.Shards.Failed > 0 {
+		err = errors.E(
+			ErrNotAllShardsReplied,
+			errors.KV("replied", results.Shards.Successful),
+			errors.KV("failed", results.Shards.Failed),
+			errors.KV("total", results.Shards.Total),
+		)
+	}
+	return results, err
+}
+
+// GetErrorFromElasticResponse checks if err is an *elastic.Error and returns an error with a formatted message.
+func GetErrorFromElasticResponse(err error) error {
+	switch e := err.(type) {
+	case *elastic.Error:
+		err = errors.New(getRootCauseFromElasticError(e.Details))
+	}
+	return err
+}
+
+func getRootCauseFromElasticError(errorDetails *elastic.ErrorDetails) string {
+	if errorDetails == nil {
+		return "unknown elastic error"
+	}
+	errors := []string{errorDetails.Type + "[" + errorDetails.Reason + "]"}
+
+	for _, rootCause := range errorDetails.RootCause {
+		errors = append(errors, rootCause.Type+"["+rootCause.Reason+"]")
+	}
+
+	return strings.Join(errors, ": ")
+}
+
+// GetElasticPaginatorFromHits gets the elastic sort in the last hit as a json string.
+func GetElasticPaginatorFromHits(hits []*elastic.SearchHit) (string, error) {
+	const op errors.Op = "elasticutil.GetElasticPaginatorFromHits"
+
+	if len(hits) == 0 {
+		return "", nil
+	}
+
+	lastHit := hits[len(hits)-1]
+	if lastHit == nil || lastHit.Sort == nil {
+		return "", nil
+	}
+	paginator, err := json.Marshal(lastHit.Sort)
+	if err != nil {
+		return "", errors.E(op, err)
+	}
+	return string(paginator), nil
+}
